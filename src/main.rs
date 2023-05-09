714a835cd3ec6b42b7d728f55d49c6f4a3296589
Lucas Brunner <LucasBrunner@gmail.com>
2023-05-09 15:14:01 -0400

    add tabs and restore terminal on close

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,12 @@ use crossterm::{
 };
 
 use chrono::prelude::*;
+use tui::{
+    layout::{Constraint, Layout},
+    style::{Modifier, Style},
+    text::{Span, Spans},
+    widgets::{Block, Borders, Paragraph, Tabs},
+};
 
 #[derive(Debug, Clone)]
 struct Pet {
@@ -32,6 +38,7 @@ enum Event<I> {
     Tick,
 }
 
+#[derive(Clone, Copy)]
 enum MenuItem {
     Home,
     Pets,
@@ -73,21 +80,81 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     });
 
     let stdout = io::stdout();
+    crossterm::execute!(io::stdout(), crossterm::terminal::EnterAlternateScreen)?;
     let backend = tui::backend::CrosstermBackend::new(stdout);
     let mut terminal = tui::Terminal::new(backend)?;
-    terminal.clear()?;
+    // terminal.clear()?;
+
+    let menu_titles = vec!["Home", "Pets", "Add", "Delete", "Quit"];
+    let mut active_menu_item = MenuItem::Home;
 
     loop {
+        _ = terminal.draw(|rect| {
+            let size = rect.size();
+            let chunks = Layout::default()
+                .direction(tui::layout::Direction::Vertical)
+                .margin(2)
+                .constraints(
+                    [
+                        Constraint::Length(3),
+                        Constraint::Min(2),
+                        Constraint::Length(3),
+                    ]
+                    .as_ref(),
+                )
+                .split(size);
+
+            let copyright = Paragraph::new("pet-CLI 2023 - all rights reserved")
+                .style(Style::default().fg(tui::style::Color::LightCyan))
+                .alignment(tui::layout::Alignment::Center)
+                .block(
+                    Block::default()
+                        .borders(Borders::ALL)
+                        .style(Style::default().fg(tui::style::Color::White))
+                        .title("Copyright")
+                        .border_type(tui::widgets::BorderType::Plain),
+                );
+
+            let menu = menu_titles
+                .iter()
+                .map(|t| {
+                    let (first, rest) = t.split_at(1);
+                    Spans::from(vec![
+                        Span::styled(
+                            first,
+                            Style::default()
+                                .fg(tui::style::Color::Yellow)
+                                .add_modifier(Modifier::UNDERLINED),
+                        ),
+                        Span::styled(rest, Style::default().fg(tui::style::Color::White)),
+                    ])
+                })
+                .collect();
+
+            let tabs = Tabs::new(menu)
+                .select(active_menu_item.into())
+                .block(Block::default().title("Menu").borders(Borders::ALL))
+                .style(Style::default().fg(tui::style::Color::White))
+                .highlight_style(Style::default().fg(tui::style::Color::Yellow))
+                .divider(Span::raw("|"));
+
+            rect.render_widget(tabs, chunks[0]);
+            rect.render_widget(copyright, chunks[2]);
+        });
+
         match rx.recv()? {
             Event::Input(event) => match event.code {
                 KeyCode::Char('q') => {
                     disable_raw_mode()?;
                     terminal.show_cursor()?;
+                    crossterm::execute!(io::stdout(), crossterm::terminal::LeaveAlternateScreen)?;
                     break;
                 }
+                KeyCode::Char('h') => active_menu_item = MenuItem::Home,
+                KeyCode::Char('p') => active_menu_item = MenuItem::Pets,
                 _ => {}
             },
-            _ => {}
+            Event::Tick => {}
         }
     }
     Ok(())
