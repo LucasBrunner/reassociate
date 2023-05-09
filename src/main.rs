4d72470d4a71f90881e0c15ecae8f473af2449fe
LucasBrunner <LucasBrunner@gmail.com>
2023-05-09 19:56:57 -0400

    db access

--- src/main.rs
+++ src/main.rs
@@ -1,138 +1,56 @@
-use std::{
-    io,
-    sync::mpsc,
-    thread,
-    time::{Duration, Instant},
-};
+use sqlx::{migrate::MigrateDatabase, Sqlite, SqlitePool};
 
-use crossterm::{
-    event::{self, Event as CEvent, KeyCode},
-    terminal::{disable_raw_mode, enable_raw_mode},
-};
+use async_std::{prelude::*, task};
 
-use tui::{layout::*, style::*, text::*, widgets::*};
+const DB_URL: &str = "sqlite://sqlite.db";
 
-enum Event<I> {
-    Input(I),
-    Tick,
+#[derive(strum_macros::Display)]
+enum DatabaseExistsResult {
+    Exists,
+    Created,
+    CouldNotBeCreated,
 }
 
-// updated comment
-#[derive(Clone, Copy)]
-enum MenuItem {
-    Home,
-    Pets,
-}
-
-impl From<MenuItem> for usize {
-    fn from(input: MenuItem) -> usize {
-        match input {
-            MenuItem::Home => 0,
-            MenuItem::Pets => 1,
+async fn db_exists() -> DatabaseExistsResult {
+    if !Sqlite::database_exists(DB_URL).await.unwrap_or(false) {
+        println!("Creating database {}", DB_URL);
+        match Sqlite::create_database(DB_URL).await {
+            Ok(_) => DatabaseExistsResult::Created,
+            Err(_) => DatabaseExistsResult::CouldNotBeCreated,
         }
+    } else {
+        DatabaseExistsResult::Exists
     }
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    enable_raw_mode().expect("can run in raw mode");
-
-    let (tx, rx) = mpsc::channel();
-    let tick_rate = Duration::from_millis(200);
-    thread::spawn(move || {
-        let mut last_tick = Instant::now();
-        loop {
-            let timeout = tick_rate
-                .checked_sub(last_tick.elapsed())
-                .unwrap_or_else(|| Duration::from_secs(0));
-
-            if event::poll(timeout).expect("poll works") {
-                if let CEvent::Key(key) = event::read().expect("can read events") {
-                    tx.send(Event::Input(key)).expect("can send events");
-                }
-            }
-
-            if last_tick.elapsed() >= tick_rate || tx.send(Event::Tick).is_ok() {
-                last_tick = Instant::now();
-            }
+fn main() {
+    match task::block_on(db_exists()) {
+        DatabaseExistsResult::CouldNotBeCreated => {
+            println!("Database could not be found or created!");
+            return;
         }
-    });
+        DatabaseExistsResult::Created => println!("Database created!"),
+        DatabaseExistsResult::Exists => println!("Database found!"),
+    };
 
-    let stdout = io::stdout();
-    crossterm::execute!(io::stdout(), crossterm::terminal::EnterAlternateScreen)?;
-    let backend = tui::backend::CrosstermBackend::new(stdout);
-    let mut terminal = tui::Terminal::new(backend)?;
-
-    let menu_titles = vec!["Home", "Pets", "Add", "Delete", "Quit"];
-    let mut active_menu_item = MenuItem::Home;
-
-    loop {
-        _ = terminal.draw(|rect| {
-            let size = rect.size();
-            let chunks = Layout::default()
-                .direction(tui::layout::Direction::Vertical)
-                .margin(2)
-                .constraints(
-                    [
-                        Constraint::Length(3),
-                        Constraint::Min(2),
-                        Constraint::Length(3),
-                    ]
-                    .as_ref(),
-                )
-                .split(size);
-
-            let copyright = Paragraph::new("pet-CLI 2023 - all rights reserved")
-                .style(Style::default().fg(tui::style::Color::LightCyan))
-                .alignment(tui::layout::Alignment::Center)
-                .block(
-                    Block::default()
-                        .borders(Borders::ALL)
-                        .style(Style::default().fg(tui::style::Color::White))
-                        .title("Copyright")
-                        .border_type(tui::widgets::BorderType::Plain),
-                );
-
-            let menu = menu_titles
-                .iter()
-                .map(|t| {
-                    let (first, rest) = t.split_at(1);
-                    Spans::from(vec![
-                        Span::styled(
-                            first,
-                            Style::default()
-                                .fg(tui::style::Color::Yellow)
-                                .add_modifier(Modifier::UNDERLINED),
-                        ),
-                        Span::styled(rest, Style::default().fg(tui::style::Color::White)),
-                    ])
-                })
-                .collect();
-
-            let tabs = Tabs::new(menu)
-                .select(active_menu_item.into())
-                .block(Block::default().title("Menu").borders(Borders::ALL))
-                .style(Style::default().fg(tui::style::Color::White))
-                .highlight_style(Style::default().fg(tui::style::Color::Yellow))
-                .divider(Span::raw("|"));
-
-            rect.render_widget(tabs, chunks[0]);
-            rect.render_widget(copyright, chunks[2]);
-        });
-
-        match rx.recv()? {
-            Event::Input(event) => match event.code {
-                KeyCode::Char('q') => {
-                    disable_raw_mode()?;
-                    terminal.show_cursor()?;
-                    crossterm::execute!(io::stdout(), crossterm::terminal::LeaveAlternateScreen)?;
-                    break;
-                }
-                KeyCode::Char('h') => active_menu_item = MenuItem::Home,
-                KeyCode::Char('p') => active_menu_item = MenuItem::Pets,
-                _ => {}
-            },
-            Event::Tick => {}
+    let db = match task::block_on(SqlitePool::connect(DB_URL)) {
+        Ok(db) => db,
+        Err(_) => {
+            println!("Could not connect to database!");
+            return;
         }
-    }
-    Ok(())
+    };
+
+    _ = task::block_on(sqlx::query!("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY NOT NULL, name VARCHAR(250) NOT NULL);").execute(&db));
+
+    let result = sqlx::query!(
+        r#"
+            SELECT name
+            FROM sqlite_schema
+            WHERE type = "table";
+        "#
+    )
+    .fetch_all(&db);
+    let result = task::block_on(result);
+    println!("{:?}", result);
 }
